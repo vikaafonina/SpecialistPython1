2d401ff2b141f67e8fe8a78feb2ff0632c4306d4
vikaafonina <86655057+vikaafonina@users.noreply.github.com>
2021-06-29 17:06:46 +0300

    Update 01_task_string.py

--- Module3/practice/string/01_task_string.py
+++ Module3/practice/string/01_task_string.py
@@ -8,3 +8,10 @@ surname = input("Фамилия: ")
 
 
 print(name, surname)
+name = input("Имя: ")
+surname = input("Фамилия: ")
+
+name = name.capitalize()
+surname = surname.capitalize()
+
+print(name, surname)
