c2b7c92f9d8bc67b20e00eeb282757f91fff7609
vikaafonina <86655057+vikaafonina@users.noreply.github.com>
2021-06-30 14:42:21 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,13 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+import random
+n = random.randint(1,10)
+lst=[]
+while len(lst)<n:
+    lst.append(random.randint(-10,10))
+summ=0
+for el in lst:
+    summ+=el
+print (lst)
+print(summ)
