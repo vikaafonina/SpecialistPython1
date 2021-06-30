b29cb00ff95bf298eaf2f3e32707213f3fb39d68
vikaafonina <86655057+vikaafonina@users.noreply.github.com>
2021-06-30 17:01:28 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -8,3 +8,10 @@ item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "cou
 dollar_rate = 74.12
 
 # TODO: your code here
+item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
+dollar_rate = 74.12
+a = float(item["price "])
+a1 = dollar_rate * a
+b = int(item["count"])
+c = a1 * b
+print(c)
