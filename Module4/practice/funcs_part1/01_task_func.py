e69b68e9177acaff28fae2cfbc399074eb1e06f5
vikaafonina <86655057+vikaafonina@users.noreply.github.com>
2021-07-01 12:14:09 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -11,3 +11,17 @@ print(max2(-10, -12))
 print(max2(2.5, 2.6))
 print(max2(-2.5, 0))
 print(max2(0, -2.5))
+
+def my_max(n1, n2):
+    if n1 < n2:
+        return n2
+    else:
+        return n1
+
+
+print(my_max(5, 6))
+print(my_max(-10, -12))
+print(my_max(2.5, 2.6))
+print(my_max(-2.5, 0))
+print(my_max(0, -2.5))
+print(my_max(5, 5))
