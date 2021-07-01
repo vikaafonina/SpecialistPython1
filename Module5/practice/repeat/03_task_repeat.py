d70d03fdacd7852a1fcaf35e62c8c92ed55c57f6
vikaafonina <86655057+vikaafonina@users.noreply.github.com>
2021-07-01 17:11:56 +0300

    Update 03_task_repeat.py

--- Module5/practice/repeat/03_task_repeat.py
+++ Module5/practice/repeat/03_task_repeat.py
@@ -3,3 +3,14 @@
 
 a = ...
 b = ...
+def palindrome(number):
+    return str(number) == str(number)[::-1]
+
+
+a = int(input("a = "))
+b = int(input("b = "))
+s = list(range(a, b + 1))
+i = 0
+for el in s:
+    i += palindrome(el)
+print(i)
